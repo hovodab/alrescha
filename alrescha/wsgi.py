43af0284b41ad1a6fb6b43ff17e343794a7f3e82
Hovhannes Dabaghyan <hovodab@mail.ru>
2019-12-12 17:50:09 +0400

    Initial.

---
+++ alrescha/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alrescha project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alrescha.settings')
+
+application = get_wsgi_application()
